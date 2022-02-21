c71c58febd0ad03441b735a4080ac9f93a639dc3
Aniket Sathe <100065028+aniket0709@users.noreply.github.com>
2022-02-21 13:28:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./MyComponents/Header"
+import Navbar from "./MyComponents/Navbar"
+import Image from "./MyComponents/image"
+import Signupform from "./MyComponents/signupform"
+import Footer from "./MyComponents/footer"
+function App() {
+  return (
+    <>
+    <Header/>
+   <Navbar/>
+   <Image/>
+   <Signupform/>
+   <Footer/>
+</>
+  );
+}
+
+export default App;
